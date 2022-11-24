e0b994a907ae60b9eefdabceb126ec36266550c1
GoldenEye GoldenEye Development <GoldenEye.Development@ibm.com>
2022-11-24 00:06:45 +0000

    chore(deps): update terraform github.ibm.com/goldeneye/secrets-manager-module to v2.6.9 (#17)

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -9,7 +9,7 @@ module "secrets_manager" {
   providers = {
     restapi.nocontent = restapi.nocontent
   }
-  source               = "git::https://github.ibm.com/GoldenEye/secrets-manager-module.git?ref=2.6.8"
+  source               = "git::https://github.ibm.com/GoldenEye/secrets-manager-module.git?ref=2.6.9"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   secrets_manager_name = "${var.prefix}-secrets-manager" #tfsec:ignore:general-secrets-no-plaintext-exposure
