b59dace4a8f5b3e3fee59f503e0d67b9b39a1660
Vincent Burckhardt <vincent.burckhardt@ie.ibm.com>
2023-05-26 17:49:46 +0100

    feat: init

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,5 +1,5 @@
 locals {
-  sm_guid   = var.existing_sm_instance_guid == null ? module.secrets_manager[0].secrets_manager_guid : var.existing_sm_instance_guid
+  sm_guid   = var.existing_sm_instance_guid == null ? ibm_resource_instance.secrets_manager[0].guid : var.existing_sm_instance_guid
   sm_region = var.existing_sm_instance_region == null ? var.region : var.existing_sm_instance_region
 }
 
@@ -10,27 +10,32 @@ module "resource_group" {
   existing_resource_group_name = var.resource_group
 }
 
-module "secrets_manager" {
-  count                = var.existing_sm_instance_guid == null ? 1 : 0
-  source               = "git::https://github.ibm.com/GoldenEye/secrets-manager-module.git?ref=3.0.0"
-  resource_group_id    = module.resource_group.resource_group_id
-  region               = local.sm_region
-  secrets_manager_name = "${var.prefix}-secrets-manager" #tfsec:ignore:general-secrets-no-plaintext-exposure
-  sm_service_plan      = "trial"
-  sm_tags              = var.resource_tags
+resource "ibm_resource_instance" "secrets_manager" {
+  count             = var.existing_sm_instance_guid == null ? 1 : 0
+  name              = "${var.prefix}-secrets-manager" #tfsec:ignore:general-secrets-no-plaintext-exposure
+  service           = "secrets-manager"
+  plan              = "trial"
+  location          = local.sm_region
+  tags              = var.resource_tags
+  resource_group_id = module.resource_group.resource_group_id
+  timeouts {
+    create = "20m" # Extending provisioning time to 20 minutes
+  }
 }
-# Best practise, use the secrets manager secret group module to create a secret group
-module "secrets_manager_secret_group" {
-  source                   = "git::https://github.ibm.com/GoldenEye/secrets-manager-secret-group-module.git?ref=2.0.1"
-  region                   = local.sm_region
-  secrets_manager_guid     = local.sm_guid
-  secret_group_name        = "${var.prefix}-certificates-secret-group"    #checkov:skip=CKV_SECRET_6: does not require high entropy string as is static value
-  secret_group_description = "secret group used for private certificates" #tfsec:ignore:general-secrets-no-plaintext-exposure
+
+# Best practice, use a secret group
+resource "ibm_sm_secret_group" "secret_group" {
+  name        = "${var.prefix}-certificates-secret-group"    #checkov:skip=CKV_SECRET_6: does not require high entropy string as is static value
+  description = "secret group used for private certificates" #tfsec:ignore:general-secrets-no-plaintext-exposure
+  region      = local.sm_region
+  instance_id = local.sm_guid
 }
+
+
 
 module "private_secret_engine" {
   count                     = var.existing_sm_instance_guid == null ? 1 : 0
-  source                    = "git::https://github.ibm.com/GoldenEye/secrets-manager-private-cert-engine-module.git?ref=2.1.0"
+  source                    = "git::https://github.com/terraform-ibm-modules/terraform-ibm-secrets-manager-private-cert-engine?ref=public"
   secrets_manager_guid      = local.sm_guid
   region                    = local.sm_region
   root_ca_name              = var.root_ca_name
@@ -46,7 +51,7 @@ module "secrets_manager_private_certificate" {
 
   cert_name              = "${var.prefix}-example-private-cert"
   cert_description       = "an example private cert"
-  cert_secrets_group_id  = module.secrets_manager_secret_group.secret_group_id
+  cert_secrets_group_id  = ibm_sm_secret_group.secret_group.secret_group_id
   cert_template          = var.certificate_template_name
   cert_common_name       = "goldeneye.appdomain.cloud"
   secrets_manager_guid   = local.sm_guid
