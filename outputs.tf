7436250d78245f2364fa8abd3dfbfc5c12e8bfeb
Valerio Bontempi <valerio.bontempi@gmail.com>
2024-06-24 16:57:14 +0200

    feat: added new output `secret_id` which will output the private cert secrets manager secret unique ID (not to be confused with the `id` output which will output the private certif secrets manager secret resource ID) (#222)
    Co-authored-by: shemau <shemau@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -2,9 +2,14 @@
 # Outputs
 ##############################################################################
 
-output "secret_id" {
-  description = "Private certificates secrets manager secret ID"
+output "id" {
+  description = "Private certificates secrets manager secret resource ID"
   value       = ibm_sm_private_certificate.secrets_manager_private_certificate.id
+}
+
+output "secret_id" {
+  description = "Private certificates secrets manager secret unique ID"
+  value       = ibm_sm_private_certificate.secrets_manager_private_certificate.secret_id
 }
 
 output "secret_crn" {
