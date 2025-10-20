099208d0199fe7fe51c762a457566a1c81499de6
ocofaigh <ocofaigh@ie.ibm.com>
2025-10-20 14:39:13 +0100

    update

--- solutions/fully-configurable/outputs.tf
+++ solutions/fully-configurable/outputs.tf
@@ -12,3 +12,8 @@ output "secret_crn" {
   description = "Private certificates secrets manager secret CRN"
   value       = module.secrets_manager_private_cert.secret_crn
 }
+
+output "secrets_manager_crn" {
+  description = "The CRN of the Secrets Manager instance"
+  value       = var.existing_secrets_manager_crn
+}
